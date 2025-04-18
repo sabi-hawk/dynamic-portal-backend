e35240191b250d8c64849b1f4f7cd21024000c80
sabi-hawk <miansabby516@gmail.com>
2025-04-18 10:52:09 +0500

    cors

--- src/api/server.ts
+++ src/api/server.ts
@@ -17,11 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
-const corsOptions = {
-  origin: ['http://example.com', 'http://localhost:3000'],
-};
 
-app.use(cors(corsOptions));
 app.disable('x-powered-by');
 app.use("/api", apiRouter);
 
